881f7337000282afc8286092f5ec0b0daf7d25b3
wsgggws <wsgggws@gmail.com>
2025-05-26 22:20:15 +0800

    fix: update scripts

--- scripts/celery-and-api.sh
+++ scripts/celery-and-api.sh
@@ -3,4 +3,4 @@ nohup celery -A celery_app beat --loglevel=info 2>&1 &
 nohup celery -A celery_app worker --pool=threads --concurrency=1 --loglevel=info 2>&1 &
 nohup celery -A celery_app worker --pool=threads --concurrency=1 --loglevel=info 2>&1 &
 
-uvicorn src.main:app --host 0.0.0.0 --port 8000
+uvicorn app.main:app --host 0.0.0.0 --port 8000
